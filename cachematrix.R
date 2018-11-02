f1bcb6c945348fc7dd6ab7cecb340d8a0de207e7
Irene Hoeppner <irene.hoeppner@isinett.de>
2018-11-02 21:03:51 +0100

    Write caching functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## creates a special vector that can be used to cache an
+## inversed matrix.
+## example for an invertible matrix: matrix(c(1, -1, 1, 2), nrow = 2, dimnames = list(c("x", "y"), c("a", "b")))
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) inv <<- solve
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Used with the vector created with makeCacheMatrix() this function
+## makes sure that the inversion fo the matrix is calculated only once
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
